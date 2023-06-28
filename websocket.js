a5c5b6cb38d510182a99c1e4438d4d80b9aae90a
MURAYAMADESU <murayamadesuhai@gmail.com>
2023-06-28 19:22:02 +0900

    細かい変更

--- websocket.js
+++ websocket.js
@@ -3,27 +3,37 @@
 const http = require('http');
 const fs = require('fs');
 
+//MIMEタイプの設定用変数
+const mime = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript"
+}
+
 //httpサーパーのポート設定
 const port = 8000;
 
-
 //htmlファイルの読み込み
-fs.readFile('index.html', 'utf-8', function(err, content){
+fs.readFile('site/index.html', 'utf-8', function(err, content){
     processFile(content);
     if(err) throw console.log(err);
 });
+
+
 
 //非同期通信の処理
 function processFile(content){
     http.createServer((request, response) => {
         response.writeHead(200, {
-            "Content-type": "text/html"
+            "Content-type": main[p]
         });
-        response.end(content);
         console.log("Sent a response");
+        response.end(content);
     }).listen(port);
     console.log("The server has started and is listening on port number:" + port)
 };
+
+
 
 
 
