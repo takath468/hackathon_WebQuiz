23b30b7854c6480cb2e94dff6b87280304c03710
MURAYAMADESU <kousuke_21n1200380@nnn.ed.jp>
2023-06-29 23:40:55 +0900

    httpServerの基礎設定

--- websocket.js
+++ websocket.js
@@ -4,55 +4,54 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-
-
+const ejs = require('ejs');
 
 //変数の定義
 //httpサーパーのポート設定
 const port = 8000;
-
+//各ファイルのデータ読み込んでレンダリングをする
+const index = fs.readFileSync('./index.html', 'UTF-8');
+const style = fs.readFileSync('./css/common.css', 'UTF-8');
+const js = fs.readFileSync('./js/Script.js', 'UTF-8');
 
 
 //functionの定義
-//htmlファイルの読み込み
-function readWebFile(src){
-    return fs.readFileSync(src);
-}
-
 
 //httpサーバーの起動、処理
-http.createServer((request,response)=>{
-    const url_parts = url.parse(request.url)
+http.createServer((request, response) => {
+    const url_parts = url.parse(request.url);
     switch (url_parts.pathname) {
         case '/':
         case '/index.html':
-            console.log('index.html');
+            console.log('htmlが読み込まれました');
             response.writeHead(200, {
                 "Content-Type": "text/html"
             });
-            response.write(readWebFile("./site/index.html"));
+            response.write(ejs.render(index));
             response.end();
             break;
-        case '/css/style.css':
-            console.log('style.css');
+        case '/css/common.css':
+            console.log('cssが読み込まれました');
             response.writeHead(200, {
                 "Content-Type": "text/css"
             });
-            response.write(readWebFile("./site/css/style.css"));
+            response.write(ejs.render(style));
             response.end();
             break;
-        case '/js/main.js':
-            console.log('main.js');
+        case '/js/Script.js':
+            console.log('jsが読み込まれました');
             response.writeHead(200, {
                 "Content-Type": "text/javascript"
             });
-            response.write(readWebFile("./site/js/main.js"));
+            response.write(ejs.render(js));
             response.end();
             break;
-            default:
+        default:
             console.log('no page');
-            response.writeHead(200, { 'Content-Type': 'text/plain' });
-            response.end();
+            response.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            response.end("404 Not Found");
             break;
     }
 }).listen(port);
