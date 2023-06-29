319b603aa88884223f481ec31b8a1898acdca188
MURAYAMADESU <kousuke_21n1200380@nnn.ed.jp>
2023-06-29 09:18:41 +0900

    MIME

--- websocket.js
+++ websocket.js
@@ -1,37 +1,76 @@
 'use strict'
+console.log("The server has started and is listening on port number:" );
+
 //各クラスの読み込み
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
-//MIMEタイプの設定用変数
-const mime = {
-    ".html": "text/html",
-    ".css": "text/css",
-    ".js": "text/javascript"
-}
 
+
+//変数の定義
+//httpサーバーの作成
+const server = http.createServer(httpSetting).listen(8000);
 //httpサーパーのポート設定
 const port = 8000;
 
+
+
+//functionの定義
 //htmlファイルの読み込み
-fs.readFile('site/index.html', 'utf-8', function(err, content){
-    processFile(content);
+function readWebFile(src){
+fs.readFile(src, 'utf-8', function(err, content){
+    return processFile(content);
     if(err) throw console.log(err);
 });
-
-
+}
 
 //非同期通信の処理
 function processFile(content){
     http.createServer((request, response) => {
-        response.writeHead(200, {
-            "Content-type": main[p]
-        });
+        response.writeHead(200, {mime});
         console.log("Sent a response");
         response.end(content);
     }).listen(port);
     console.log("The server has started and is listening on port number:" + port)
 };
+
+
+
+//httpサーバーの設定
+function httpSetting(request, response){
+    const url_parts = new URL(request.url);
+    switch(url_parts.pathname){
+        case '/':
+        case '/index.html':
+            response.writeHead(200,{
+                "Content-Type": "text/html"
+            });
+            response.write(readWebFile('./site/index.html'));
+            response.end();
+        break;
+        case '/css/style.css':
+            response.writeHead(200,{
+                "Content-Type": "text/css"
+            });
+            response.write(readWebFile('./site/css/style.css'));
+            response.end();
+        break;
+        case '/js/main.js':
+            response.writeHead(200,{
+                "Content-Type": "text/javascript"
+            });
+            response.write(readWebFile('./site/js/main.js'));
+            response.end();
+        break;
+        default:
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('お探しのページは見つかりません。');
+        break;
+    }
+}
+
+
 
 
 
