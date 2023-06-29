01b59dc03cc453d53f9b0b09371ca83af6f463c6
MURAYAMADESU <murayamadesuhai@gmail.com>
2023-06-29 15:19:40 +0900

    修正

--- websocket.js
+++ websocket.js
@@ -9,7 +9,7 @@ const url = require('url');
 
 //変数の定義
 //httpサーパーのポート設定
-const port = 8900;
+const port = 8000;
 
 
 
@@ -22,30 +22,35 @@ function readWebFile(src){
 
 //httpサーバーの起動、処理
 http.createServer((request,response)=>{
-    switch (request.url.pathname) {
+    const url_parts = url.parse(request.url)
+    switch (url_parts.pathname) {
         case '/':
         case '/index.html':
+            console.log('index.html');
             response.writeHead(200, {
                 "Content-Type": "text/html"
             });
-            response.write(readWebFile('/site/index.html'));
+            response.write(readWebFile("./site/index.html"));
             response.end();
             break;
         case '/css/style.css':
+            console.log('style.css');
             response.writeHead(200, {
                 "Content-Type": "text/css"
             });
-            response.write(readWebFile('./site/css/style.css'));
+            response.write(readWebFile("./site/css/style.css"));
             response.end();
             break;
         case '/js/main.js':
+            console.log('main.js');
             response.writeHead(200, {
                 "Content-Type": "text/javascript"
             });
-            response.write(readWebFile('./site/js/main.js'));
+            response.write(readWebFile("./site/js/main.js"));
             response.end();
             break;
-        default:
+            default:
+            console.log('no page');
             response.writeHead(200, { 'Content-Type': 'text/plain' });
             response.end();
             break;
